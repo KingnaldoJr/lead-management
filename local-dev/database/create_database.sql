c27ee0c227528762d67ad27461f65f7d7ab871df
Reinaldo Junior <reinaldomalinauskasjr@gmail.com>
2024-10-23 21:49:22 -0300

    initial commit

---
+++ local-dev/database/create_database.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE lead_management;
+
+CREATE TABLE IF NOT EXISTS addresses (
+    id UUID NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    number VARCHAR(10) NOT NULL,
+    complement VARCHAR(100) NOT NULL,
+    neighborhood VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    zip INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS leads (
+    id UUID NOT NULL,
+    address_id UUID NOT NULL,
+    name varchar(100) NOT NULL,
+    email varchar(255) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (address_id) REFERENCES addresses (id)
+);
+
+CREATE TABLE IF NOT EXISTS zipcode_search_history (
+  id UUID NOT NULL,
+  zipcode INTEGER NOT NULL,
+  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  response JSONB,
+  PRIMARY KEY (id)
+);
---
+++ src/main/java/dev/rmjr/leadregistry/LeadregistryApplication.java
@@ -0,0 +1,13 @@
+package dev.rmjr.leadregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadregistryApplication.class, args);
+	}
+
+}
